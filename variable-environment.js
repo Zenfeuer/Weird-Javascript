82e80cab596ed16599ddaf33bebb46481b2a4d05
Darwing <darwing.her69@gmail.com>
2018-11-19 20:05:45 -0600

    Improvement over variable enviroment doc
    - Improvement over variable enviroment doc.

--- variable-environment.js
+++ variable-environment.js
@@ -1,7 +1,9 @@
 /***********************************************************************************************************************
  * VARIABLE ENVIRONMENT
  * 
- * Variable environment refers to where the variables live and how the relate to each other in memory.
+ * Variable environment refers to where the variables live and how the relate to each other in memory. So, when a
+ * varable is going to be used, it matters where it was defined, so if a global variable and a variable defined in a
+ * function have the same name, they never collide because the live in different spaces in memory.
  *
  **********************************************************************************************************************/
 
