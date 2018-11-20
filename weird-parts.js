25843f26c17360eab2fbb80a305a7aca8b33a42e
Darwing <darwing.her69@gmail.com>
2018-11-19 19:56:11 -0600

    Update weird-parts.js
    - Variable environment doc

--- weird-parts.js
+++ weird-parts.js
@@ -41,4 +41,38 @@ function myFunc2 ()
     console.log('Called myFunc2!');
 }
 
+/***********************************************************************************************************************
+ * VARIABLE ENVIRONMENT AND SCOPE CHAIN
+ * 
+ * Variable environment refers to where the variables live and how the relate to each other in memory.
+ *
+ * 
+ *
+ **********************************************************************************************************************/
 
+function b () 
+{
+	// This variable lives in this function, so it nevers collide with the global definition and neither with the
+	// function a() that invoke this function, it is created in a diferent execution context for this function when it 
+	// is executed. So, it prints the value 'undefined'.
+	var myVar;
+    console.log(myVar);
+}
+
+function a () 
+{
+	// This variable lives in this function, so it nevers collide with the global definition, it is created in a
+	// diferent execution context for this function when it is executed. So, it prints the value '2'.
+	var myVar = 2;
+    console.log(myVar);
+	b();
+}
+
+var myVar = 1;
+console.log(myVar);
+
+a();
+
+// This still prints the value '1', because the invocation of the functions does not affects the value of the global
+// variable myVar thanks to the variable environment.
+console.log(myVar);
