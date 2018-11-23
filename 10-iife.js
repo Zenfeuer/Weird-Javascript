64fa663e2ac7219e2f9ff11140e4e857658c24de
Darwing <darwing.her69@gmail.com>
2018-11-22 23:22:40 -0600

    IIFE doc
    - IIFE doc

---
+++ 10-iife.js
@@ -0,0 +1,37 @@
+/***********************************************************************************************************************
+ * IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE)
+ *
+ * An IIFE is a function expression that is invoked immediately where is defined/created.
+ *
+ **********************************************************************************************************************/
+
+// Using an Immediately Invoked Function Expression (IIFE)
+// Note that darthVader now contains what returns the function and not the function itself, so using darthVader() throws
+// an error because in this case is a string.
+var darthVader = function (quote)
+{
+
+    return 'Darth Vader said: ' + quote;
+
+// Note that function is invoked on the fly passing the parameters needed
+}('The circle is now complete. When I left you, I was but the learner. Now I am the master.');
+
+console.log(darthVader);
+
+var harveyDentQuote = 'You either die a hero or you live long enough to see yourself become the villain.';
+
+// IIFE in anonymous function
+(function (quote) {
+
+    console.log('IIFE anonymous function!')
+    console.log(quote);
+    
+}(harveyDentQuote));
+
+// Another IIFE in anonymous function
+(function (number1, number2) {
+    console.log('Result is -> ' + (number1 * number2));
+
+// This is also a valid way to pass parameters to an anonymous IIFE.
+})(5, 7);
+
