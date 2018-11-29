d53b63012fea3a12d49a727f3a69d5e5a040bbe6
Darwing Hernandez <darwing.her69@gmail.com>
2018-11-28 18:19:43 -0600

    Update prototypal inheritance doc
    - Update prototypal inheritance doc

--- 0-weird-parts-all.js
+++ 0-weird-parts-all.js
@@ -1061,11 +1061,11 @@ console.log(arr5);
  * that is simpler, flexible, extensible and easy to understand. Javascript's object system is based on PROTOTYPES, not
  * classes. Prototypal inheritance is no more than objects inherits from other objects.
  *
+ * All objects has an special property called 'proto', that is another object and helps to implement the prototypal
+ * inheritance. Everything that is not a primitive (basic object, function and array) they all have a prototype, except
+ * one thing, the base object in Javascript => Object {}. This base object is in the bottom on prototype chain.
+ *
  **********************************************************************************************************************/
-
-///// ********
-// @TODO: Put examples of the proto of object, function an array and to see the scope chain
-///// ********
 
 // NOTE: This example is for demo purposes, NEVER assign the __proto__ directly, this can bring performance issues.
 
@@ -1116,6 +1116,39 @@ person.getFormalFullName = function() {
 // Both can use getFormalFullName() method
 console.log(john.getFormalFullName());
 console.log(jane.getFormalFullName());
+
+var a = {};
+var b = function () {};
+var c = [];
+
+// The prototype of an object is Object {} and contains basic methods and properties that can be used for all objects 
+// such as toString() or hasOwnProperty().
+console.log(a.__proto__);
+
+// The prototype of a function is function Empty () {} and contains basic methods and properties that can be used for 
+// all functions such as call(), bind(), apply(), arguments, etc. The prototype of Empty() is Object {} because is on 
+// the bottom of prototype chain.
+console.log(b.__proto__);
+
+// The prototype of an array is [] and contains basic methods and properties that can be used for all arrays such as 
+// push(), length, etc. The prototypeo of [] is Object {} because is on the bottom of prototype chain.
+console.log(c.__proto__);
+
+console.log('----- Reflection example -----');
+
+// Objects in Javascript can look up to their own properties and methods, this is called reflection. The following 
+// example, the loop iterates over all properties and methods defined in john object, including the ones defined in the
+// prototype. If you want to list/change the properties and methods of the object itself, you must used the method
+// hasOwnProperty(propName) to apply the filter.
+for (var prop in john)
+{
+    if (john.hasOwnProperty(prop))
+    {
+        console.log(prop + ': ' + john[prop]);
+    }
+}
+
+console.log('----- End Reflection example -----');
 
 
 
