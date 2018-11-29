c37f7419ed40f2aadad53e38a901a8900a1ae6d3
Darwing Hernandez <darwing.her69@gmail.com>
2018-11-28 18:58:58 -0600

    Update Function Constructor doc
    - Update Function Constructor doc

--- 0-weird-parts-all.js
+++ 0-weird-parts-all.js
@@ -1131,7 +1131,7 @@ console.log(a.__proto__);
 console.log(b.__proto__);
 
 // The prototype of an array is [] and contains basic methods and properties that can be used for all arrays such as 
-// push(), length, etc. The prototypeo of [] is Object {} because is on the bottom of prototype chain.
+// push(), length, etc. The prototype of [] is Object {} because is on the bottom of prototype chain.
 console.log(c.__proto__);
 
 console.log('----- Reflection example -----');
@@ -1155,7 +1155,17 @@ console.log('----- End Reflection example -----');
 /***********************************************************************************************************************
  * FUNCTION CONSTRUCTORS, OPERATOR NEW AND PROTOTYPE
  *
- * There is another way to create objects in Javascript using function constructors and the operator 'new'.
+ * There is another way to create objects in Javascript using function constructors and the operator 'new'. A function
+ * constructor sets the properties and methods for an object via 'this' keyword, because the 'new' operator creates the
+ * new object and set the 'this' to point to that new object when the function constructor is executed. Also, 'new'
+ * operator sets the prototype of that empty object to the prototype property of the function that you then call.
+ *
+ * All functions have access to an special property called prototype, used only by the 'new' operator, so this property
+ * is used by function constructors. This is a CONFUSING name, because the 'prototype' property of a function IS NOT the
+ * property of the function.
+ *
+ * For memory efficiency, it is better to define the methods in the prototype and not in the function constructor, and
+ * only define the properties in the function constructor.
  *
  **********************************************************************************************************************/
 
@@ -1170,11 +1180,22 @@ function Person(firstname, lastname)
     this.firstname = firstname;
     this.lastname = lastname;
 
+    // If you define a method in the function constructor, every object is going to have its own copy of this method, 
+    // taking much more memory space.
+    //this.fn = function () {};
+
     console.log('This function constructor is invoked.');
 
     // If you return something in a function constructor, so the variable will have the returned value and not the new
     // object
     //return "This is Sparta!!!!";
+}
+
+// This add getFullName() to the prototype Person, so the objects created with the function constructor Person are going
+// to have access to this method because prototype chain.
+Person.prototype.getFullName = function ()
+{
+    return this.firstname + ' ' + this.lastname;
 }
 
 // 'new' operator creates an empty object, then calls the function constructor Person with 'this' pointing to the new 
@@ -1183,7 +1204,20 @@ function Person(firstname, lastname)
 var luke = new Person('Luke', 'Skywalker');
 console.log(luke);
 
+// luke object has access to getFullName() because its prototype is Person (prototype chain).
+console.log(luke.getFullName());
+
 var leia = new Person('Leia', 'Organa');
 console.log(leia);
+
+// This add getFormalFullName() to the prototype Person, so the objects created with the function constructor Person are
+// going to have access to this method because prototype chain.
+Person.prototype.getFormalFullName = function ()
+{
+    return this.lastname + ', ' + this.firstname;
+}
+
+// leia object has access to getFullName() because its prototype is Person (prototype chain).
+console.log(leia.getFormalFullName());
 
 
--- 15-function-constructors-prototype.js
+++ 15-function-constructors-prototype.js
@@ -1,7 +1,17 @@
 /***********************************************************************************************************************
  * FUNCTION CONSTRUCTORS, OPERATOR NEW AND PROTOTYPE
  *
- * There is another way to create objects in Javascript using function constructors and the operator 'new'.
+ * There is another way to create objects in Javascript using function constructors and the operator 'new'. A function
+ * constructor sets the properties and methods for an object via 'this' keyword, because the 'new' operator creates the
+ * new object and set the 'this' to point to that new object when the function constructor is executed. Also, 'new'
+ * operator sets the prototype of that empty object to the prototype property of the function that you then call.
+ *
+ * All functions have access to an special property called prototype, used only by the 'new' operator, so this property
+ * is used by function constructors. This is a CONFUSING name, because the 'prototype' property of a function IS NOT the
+ * property of the function.
+ *
+ * For memory efficiency, it is better to define the methods in the prototype and not in the function constructor, and
+ * only define the properties in the function constructor.
  *
  **********************************************************************************************************************/
 
@@ -16,11 +26,22 @@ function Person(firstname, lastname)
     this.firstname = firstname;
     this.lastname = lastname;
 
+    // If you define a method in the function constructor, every object is going to have its own copy of this method, 
+    // taking much more memory space.
+    //this.fn = function () {};
+
     console.log('This function constructor is invoked.');
 
     // If you return something in a function constructor, so the variable will have the returned value and not the new
     // object
     //return "This is Sparta!!!!";
+}
+
+// This add getFullName() to the prototype Person, so the objects created with the function constructor Person are going
+// to have access to this method because prototype chain.
+Person.prototype.getFullName = function ()
+{
+    return this.firstname + ' ' + this.lastname;
 }
 
 // 'new' operator creates an empty object, then calls the function constructor Person with 'this' pointing to the new 
@@ -29,7 +50,20 @@ function Person(firstname, lastname)
 var luke = new Person('Luke', 'Skywalker');
 console.log(luke);
 
+// luke object has access to getFullName() because its prototype is Person (prototype chain).
+console.log(luke.getFullName());
+
 var leia = new Person('Leia', 'Organa');
 console.log(leia);
+
+// This add getFormalFullName() to the prototype Person, so the objects created with the function constructor Person are
+// going to have access to this method because prototype chain.
+Person.prototype.getFormalFullName = function ()
+{
+    return this.lastname + ', ' + this.firstname;
+}
+
+// leia object has access to getFullName() because its prototype is Person (prototype chain).
+console.log(leia.getFormalFullName());
 
 
