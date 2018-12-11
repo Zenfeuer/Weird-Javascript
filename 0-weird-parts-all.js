3cdd513b30d863cf9e9a317c3f840fb44f3df4a2
Darwing Hernández <darwing.her69@gmail.com>
2018-12-10 23:52:36 -0600

    Update 0-weird-parts-all.js
    Improved comments

--- 0-weird-parts-all.js
+++ 0-weird-parts-all.js
@@ -1174,9 +1174,9 @@ console.log(arr5);
  *
  * Each programming language manages the inheritance in different way. Javascript works with prototypal inheritance, 
  * that is simpler, flexible, extensible and easy to understand. Javascript's object system is based on PROTOTYPES, not
- * classes. Prototypal inheritance is no more than objects inherits from other objects.
+ * classes. Prototypal inheritance is not more than objects 'inherits' from other objects.
  *
- * All objects has an special property called 'proto', that is another object and helps to implement the prototypal
+ * All objects have an special property called 'proto', that is another object and helps to implement the prototypal
  * inheritance. Everything that is not a primitive (basic object, function and array) they all have a prototype, except
  * one thing, the base object in Javascript => Object {}. This base object is in the bottom on prototype chain.
  *
@@ -1198,9 +1198,12 @@ var john = {
     lastname: 'Cena'
 }
 
-// DON'T DO THIS EVER! For demo purposes only!!!
-// In this case, we are forcing that john object inherits from person object. firstname and lastname properties are
-// already defined in john object, so it does not inherits those properties from person (neither the values).
+/**
+ * DON'T DO THIS EVER! For demo purposes only!!!
+ *
+ * We are forcing that john object 'inherits' from person object. firstname and lastname properties are already defined
+ * in john object, so it does not inherits those properties from person (neither the values).
+ */
 john.__proto__ = person;
 
 // Now john object can access getFullName() method. This prints out 'John Cena'.
@@ -1213,22 +1216,31 @@ var jane = {
     firstname: 'Jane'   
 }
 
-// DON'T DO THIS EVER! For demo purposes only!!!
-// In this case, we are forcing that jane object inherits from person object. firstname property is already defined in 
-// jane object, so it does not inherits that property from person (neither the value).
+/**
+ * DON'T DO THIS EVER! For demo purposes only!!!
+ * 
+ * We are forcing that jane object 'inherits' from person object. firstname property is already defined in jane object, 
+ * so it does not inherits that property from person (neither the value).
+ */
 jane.__proto__ = person;
 
-// Now jane object can access getFullName() method. This prints out 'Jane Default', because jane object has not defined
-// lastname property, so by scope chain it uses the defined one from person object.
+/** 
+ * Now jane object can access getFullName() method. This prints out 'Jane Default', because jane object has not defined
+ * lastname property, so by prototype chain it uses the defined one from person object.
+ */
 console.log(jane.getFullName());
 
-// Mutating an object that is being inherited by other objects, those objects are also going to see/access the new
-// added properties and methods, because of scope chain.
+/**
+ * IMPORTANT NOTE
+ *
+ * Mutating an object that is being inherited by other objects, those objects are also going to see/access the new added
+ * properties and methods, because of prototype chain.
+ */
 person.getFormalFullName = function() {
     return this.lastname + ', ' + this.firstname;   
 }
 
-// Both can use getFormalFullName() method
+// Both can use getFormalFullName() method.
 console.log(john.getFormalFullName());
 console.log(jane.getFormalFullName());
 
@@ -1236,17 +1248,29 @@ var a = {};
 var b = function () {};
 var c = [];
 
-// The prototype of an object is Object {} and contains basic methods and properties that can be used for all objects 
-// such as toString() or hasOwnProperty().
+/**
+ * NOTE: Prototype of an Object
+ *
+ * The prototype of an object is Object {} and it contains basic methods and properties that can be used for all objects 
+ * such as toString() or hasOwnProperty().
+ */
 console.log(a.__proto__);
 
-// The prototype of a function is function Empty () {} and contains basic methods and properties that can be used for 
-// all functions such as call(), bind(), apply(), arguments, etc. The prototype of Empty() is Object {} because is on 
-// the bottom of prototype chain.
+/**
+ * NOTE: Prototype of a Function
+ *
+ * The prototype of a function is function Empty () {} and it contains basic methods and properties that can be used for 
+ * all functions such as call(), bind(), apply(), arguments, etc. The prototype of Empty() is Object {} because is on 
+ * the bottom of prototype chain.
+ */
 console.log(b.__proto__);
 
-// The prototype of an array is [] and contains basic methods and properties that can be used for all arrays such as 
-// push(), length, etc. The prototype of [] is Object {} because is on the bottom of prototype chain.
+/**
+ * NOTE: Prototype of an Array
+ *
+ * The prototype of an array is [] and contains basic methods and properties that can be used for all arrays such as 
+ * push(), length, etc. The prototype of [] is Object {} because is on the bottom of prototype chain.
+ */
 console.log(c.__proto__);
 
 console.log('----- Reflection example -----');
