bc173f219de2eab361aecef4a949c112b790fc6b
Darwing Hernández <darwing.her69@gmail.com>
2019-04-24 00:15:58 -0500

    Update 1-global-execution-context.js
    - Improvements

--- 1-global-execution-context.js
+++ 1-global-execution-context.js
@@ -1,19 +1,29 @@
-/***********************************************************************************************************************
+/*******************************************************************************
+ *
  * GLOBAL EXECUTION CONTEXT, HOISTING AND EXECUTION
- * 
- * Container/wrapper that contains the global object that in web is the window  object and 'this' that points to the 
- * global object. If there is an outer environment, a reference to that is also created.
  *
- * Also contains the variables defined as global and the definitions of functions to be used when the code is running.
+ * ****************************************************************************/
+
+/**
+ * GLOBAL EXECUTION: Container/wrapper that contains the global object that in 
+ * web is the window  object and 'this' that points to the global object. If
+ * there is an outer environment, a reference to that is also created.
  *
- * During creation phase, all the global variables and functions are HOISTED, i.e., it setups the memory space for those
- * functions and variables.
- *
- * When a function is invoked, a new execution context is stacked in the execution stack, this new execution context is
- * similar to the global execution context, so the outer environment is the context where that function lives, mainly is
- * the global execution context.
- *
- **********************************************************************************************************************/
+ * Also contains the variables defined as global and the definitions of 
+ * functions to be used when the code is running.
+ */
+
+/**
+ * HOISTING: During creation phase, all the global variables and functions are 
+ * "hoisted", i.e., it setups the memory space for those functions and variables
+ */
+
+/**
+ * EXECUTION: When a function is invoked, a new execution context is stacked in 
+ * the execution stack, this new execution context is similar to the global 
+ * execution context, so the outer environment is the context where that 
+ * function lives, mainly is the global execution context.
+ */
 
 // Global variable. The value "Hello World!" is not assigned during creation phase but when this line is executed.
 var foo = "Hello World!";
